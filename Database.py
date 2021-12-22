8961d4808b79af6c1a79746189fd98200109aead
PvtMcBagel <mcbagel@Live.com>
2021-12-22 17:10:21 -0500

    initial push

---
+++ Database.py
@@ -0,0 +1,9 @@
+#Alright, here goes. We're back to programming. The resource I'm looking at for this project to start with is right here - 
+#https://www.freecodecamp.org/news/connect-python-with-sql/
+
+#Hopefully this all goes well, the plan is for this to be a database of my games / movies / tv shows / comics / books / all of it
+#I want this to be accessable via my phone eventually so that I can check this database while out shopping.
+
+import _mysql_connector
+from _mysql_connector import Error
+import pandas as pd
